6bae71a90803131514d83bf30e9154d52c1843f9
bernouilli90 <bernouilli90@users.noreply.github.com>
2025-10-13 12:16:25 +0200

    Remove test files from repository

--- test_regex.py
+++
@@ -1,5 +0,0 @@
-from models import generate_channel_name_regex
-print('Testing regex generation:')
-print('DAZN LALIGA ->', repr(generate_channel_name_regex('DAZN LALIGA')))
-print('ESPN PLUS ->', repr(generate_channel_name_regex('ESPN PLUS')))
-print('Single word ->', repr(generate_channel_name_regex('MOVISTAR')))
--- test_resolution.py
+++
@@ -1,7 +0,0 @@
-from models import StreamMatcher
-
-# Test different resolutions
-test_resolutions = ['640x480', '720x576', '1280x720', '1920x1080', '3840x2160']
-for res in test_resolutions:
-    normalized = StreamMatcher._normalize_resolution(res)
-    print(f'{res} -> {normalized}')
